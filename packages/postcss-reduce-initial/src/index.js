0b077a35b2bc57a4b9ba83fc40ae58d4c9ce6f48
Ludovico Fischer <livrerie@gmail.com>
2022-01-19 23:32:39 +0100

    feat(postcs-reduce-initial): add types

--- packages/postcss-reduce-initial/src/index.js
+++ packages/postcss-reduce-initial/src/index.js
@@ -1,17 +1,20 @@
 import browserslist from 'browserslist';
 import { isSupported } from 'caniuse-api';
-import fromInitial from '../data/fromInitial.json';
-import toInitial from '../data/toInitial.json';
+import fromInitial from '../src/data/fromInitial.json';
+import toInitial from '../src/data/toInitial.json';
 
 const initial = 'initial';
 
 // In most of the browser including chrome the initial for `writing-mode` is not `horizontal-tb`. Ref https://github.com/cssnano/cssnano/pull/905
 const defaultIgnoreProps = ['writing-mode', 'transform-box'];
-
+/**
+ * @type {import('postcss').PluginCreator<void>}
+ * @return {import('postcss').Plugin}
+ */
 function pluginCreator() {
   return {
     postcssPlugin: 'postcss-reduce-initial',
-
+    /** @param {import('postcss').Result & {opts: browserslist.Options & {ignore?: string[]}}} result */
     prepare(result) {
       const resultOpts = result.opts || {};
       const browsers = browserslist(null, {
@@ -36,7 +39,8 @@ function pluginCreator() {
             if (
               initialSupport &&
               Object.prototype.hasOwnProperty.call(toInitial, lowerCasedProp) &&
-              decl.value.toLowerCase() === toInitial[lowerCasedProp]
+              decl.value.toLowerCase() ===
+                toInitial[/** @type {keyof toInitial} */ (lowerCasedProp)]
             ) {
               decl.value = initial;
               return;
@@ -44,12 +48,13 @@ function pluginCreator() {
 
             if (
               decl.value.toLowerCase() !== initial ||
-              !fromInitial[lowerCasedProp]
+              !fromInitial[/** @type {keyof fromInitial}*/ (lowerCasedProp)]
             ) {
               return;
             }
 
-            decl.value = fromInitial[lowerCasedProp];
+            decl.value =
+              fromInitial[/** @type {keyof fromInitial} */ (lowerCasedProp)];
           });
         },
       };
