f616e9840877bc94498b2aa7bdab03e4dea85ef3
Ludovico Fischer <livrerie@gmail.com>
2022-01-27 21:46:37 +0100

    feat(cssnano-preset-default): add types

--- packages/cssnano-preset-default/src/index.js
+++ packages/cssnano-preset-default/src/index.js
@@ -41,6 +41,35 @@ import postcssNormalizeUnicode from 'postcss-normalize-unicode';
 import postcssNormalizeDisplayValues from 'postcss-normalize-display-values';
 import postcssNormalizeTimingFunctions from 'postcss-normalize-timing-functions';
 import { rawCache } from 'cssnano-utils';
+/** @typedef {{
+discardComments?: false | import('postcss-discard-comments').Options & { exclude?: true},
+reduceInitial?:  false | { exclude?: true}
+minifyGradients?:  false | { exclude?: true}
+svgo?: false | import('postcss-svgo').Options & { exclude?: true},
+reduceTransforms?:  false | { exclude?: true}
+convertValues?: false | import('postcss-convert-values').Options & { exclude?: true},
+calc?: false | import('postcss-calc').PostCssCalcOptions & { exclude?: true},
+colormin?: false | Record<string, any> & { exclude?: true},
+orderedValues?: false | { exclude?: true},
+minifySelectors?: false | { exclude?: true},
+minifyParams?: false | { exclude?: true},
+normalizeCharset?: false | import('postcss-normalize-charset').Options & { exclude?: true},
+minifyFontValues?: false | import('postcss-minify-font-values').Options & { exclude?: true},
+normalizeUrl?: false | import('postcss-normalize-url').Options & { exclude?: true},
+mergeLonghand?: false | { exclude?: true},
+discardDuplicates?: false | { exclude?: true},
+discardOverridden?: false | { exclude?: true},
+normalizeRepeatStyle?: false | { exclude?: true},
+mergeRules?: false | { exclude?: true},
+discardEmpty?: false | { exclude?: true},
+uniqueSelectors?: false | { exclude?: true},
+normalizeString?: false | import('postcss-normalize-string').Options & { exclude?: true},
+normalizePositions?: false | { exclude?: true},
+normalizeWhitespace?: false| { exclude?: true},
+normalizeUnicode?: false | { exclude?: true},
+normalizeDisplayValues?: false | { exclude?: true},
+normalizeTimingFunctions?: false | { exclude?: true},
+rawCache?: false | { exclude?: true}}} Options */
 
 const defaultOpts = {
   convertValues: {
@@ -53,10 +82,13 @@ const defaultOpts = {
     keepOverrides: true,
   },
 };
-
+/**
+ * @param {Options} opts
+ * @return {{plugins: [import('postcss').PluginCreator<any>, false | Record<string, any> | undefined][]}}
+ */
 export default function defaultPreset(opts = {}) {
   const options = Object.assign({}, defaultOpts, opts);
-
+  /** @type {[import('postcss').PluginCreator<any>, false | object | undefined][]} **/
   const plugins = [
     [postcssDiscardComments, options.discardComments],
     [postcssMinifyGradients, options.minifyGradients],
